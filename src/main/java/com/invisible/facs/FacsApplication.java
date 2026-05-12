5515e698c3ed1edb62a4046287c76725173672db
aniknandi.it@gmail.com <aniknandi.it@gmail.com>
2026-05-12 23:26:53 +0600

    first commit

---
+++ src/main/java/com/invisible/facs/FacsApplication.java
@@ -0,0 +1,13 @@
+package com.invisible.facs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacsApplication.class, args);
+	}
+
+}
