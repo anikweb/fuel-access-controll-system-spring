d94931eca84aa79b73777946adb2619dbe981bc1
aniknandi.it@gmail.com <aniknandi.it@gmail.com>
2026-05-14 01:43:59 +0600

    Add WebMvcConfig for resource handling with StorageProperties

---
+++ src/main/java/com/invisible/facs/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.invisible.facs.config;
+
+import com.invisible.facs.storage.StorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(StorageProperties.class)
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private StorageProperties storageProperties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(storageProperties.getUrlPrefix() + "/**")
+                .addResourceLocations("file:" + storageProperties.getDir() + "/");
+    }
+}
